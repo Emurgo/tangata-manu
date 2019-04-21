0481b3b9ec0efefe26beccd1c786b9bbf2622b79
Ruslan Dudin <vantuz-subhuman@yandex.ru>
2019-04-22 01:31:56 +0900

    Fixed code styling a bit

--- src/entities/genesis-provider.js
+++ src/entities/genesis-provider.js
@@ -21,7 +21,8 @@ const structUtxo = (
   receiver,
   amount,
   utxoHash,
-  txIndex = 0) => ({
+  txIndex = 0,
+) => ({
   utxo_id: `${utxoHash}${txIndex}`,
   tx_hash: utxoHash,
   tx_index: txIndex,
